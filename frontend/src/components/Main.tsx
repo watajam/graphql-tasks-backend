ba30c59deffb78d595cbef458900f112ed8cf8c0
watajam <wata124coron@icloud.com>
2023-02-28 22:50:59 +0900

    init

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,37 @@
+import { useQuery } from '@apollo/client';
+import { Stack, Typography } from '@mui/material';
+import jwtDecode from 'jwt-decode';
+import { GET_TASKS } from '../queries/taskQueries';
+import { Payload } from '../types/payload';
+import { Task } from '../types/task';
+import AddTask from './AddTask';
 import Header from './Header'
+import TaskTable from './TaskTable'
+import Loading from './Loading'
 
 const Main = () => {
+  const token = localStorage.getItem('token');
+  const decodedToken = jwtDecode<Payload>(token!);
+  const userId = decodedToken.sub;
+
+  const { loading, data, error } = useQuery<{getTasks: Task[]}>(GET_TASKS, {
+    variables: {userId}
+  });
+  
   return (
     <>
       <Header />
-      <div>Main</div>
+      <Stack spacing={4} direction='column' m={8} alignItems='center'>
+        { loading && <Loading /> }
+        { error && <Typography color='red'>エラーが発生しました</Typography>}
+        { !loading && !error && (
+          <>
+            <AddTask userId={userId} />
+            <TaskTable tasks={data?.getTasks} userId={userId}/> 
+          </>
+        )}
+        
+      </Stack>
     </>
   )
 }
